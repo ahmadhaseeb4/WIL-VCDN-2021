ed031c20e0cd6379dc69e5e112ef1b453b2460e7
Muhammad Mubeen Hamid <muhammadmubeenhamid@gmail.com>
2021-11-19 01:11:10 +0200

    Post CR/P-Like/Unlike/F-Comment/CRD/F-Articles/Videos/FAQ/CR/P - RF

--- lib/pages/contact_us/contact_us.dart
+++ lib/pages/contact_us/contact_us.dart
@@ -1,13 +1,16 @@
 import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:octo_image/octo_image.dart';
+import 'package:pwot/models/image.dart';
 import 'package:pwot/models/userModel.dart';
 import 'package:pwot/pages/contact_us/widget/d_add_therapist.dart';
 import 'package:pwot/pages/contact_us/widget/m_add_therapist.dart';
 import 'package:pwot/utility/app_colors.dart';
 import 'package:flash/flash.dart';
-
+import 'package:firebase/firebase.dart' as FB;
+import 'dart:html' as HTML;
 
 
 class ContactUs extends StatefulWidget {
@@ -18,6 +21,7 @@ class ContactUs extends StatefulWidget {
 }
 
 class _ContactUsState extends State<ContactUs> {
+  HTML.File? file;
   bool loader = false;
   List<UserModel> therapists = [];
 
@@ -52,7 +56,7 @@ class _ContactUsState extends State<ContactUs> {
     return Scaffold(
       body: Container(
         color: AppColor.bgSideMenu,
-        child: loader ? Center(child: CircularProgressIndicator(color: AppColor.bgSideMenu,),): Container(
+        child: loader ? Center(child: CircularProgressIndicator(color: AppColor.yellow,),): Container(
           decoration: BoxDecoration(
             borderRadius: const BorderRadius.all(Radius.circular(10)),
             color: AppColor.bgColor,
@@ -63,13 +67,14 @@ class _ContactUsState extends State<ContactUs> {
               children: [
                 Expanded(
                     child: ListView.builder(
+                      padding: EdgeInsets.symmetric(horizontal: 100),
                         itemBuilder: (_, i) {
                           return Card(
                             elevation: 5,
                             shadowColor: AppColor.bgSideMenu,
                             child: Container(
                               decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(5)), color: AppColor.white,),
-                              height: 200,
+                              height: 300,
                               child: loader
                                   ? Center(
                                   child: CircularProgressIndicator(
@@ -80,6 +85,7 @@ class _ContactUsState extends State<ContactUs> {
                                   Expanded(
                                       child: Container(
                                         padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+                                        height: double.infinity,
                                         width: double.infinity,
                                         child: Card(
                                           color: AppColor.bgColor,
@@ -95,11 +101,121 @@ class _ContactUsState extends State<ContactUs> {
                                   Expanded(
                                       child: Column(
                                         children: [
-                                          Text("Name"),
+                                          SizedBox(height: 25,),
+                                          Text("Name", style: TextStyle(fontWeight: FontWeight.bold),),
                                           Text(therapists[i].name),
                                           SizedBox(height: 15,),
-                                          Text("Contact"),
+                                          Text("Contact", style: TextStyle(fontWeight: FontWeight.bold),),
                                           Text(therapists[i].contact),
+                                          Row(
+                                            children: [
+                                              SizedBox(width: 10,),
+                                              Expanded(
+                                                child: Container(
+                                                  margin: EdgeInsets.symmetric(vertical: 35),
+                                                  decoration: BoxDecoration(
+                                                      borderRadius: const BorderRadius.all(Radius.circular(5.0)),
+                                                      color: AppColor.yellow
+                                                  ),
+                                                  child: MaterialButton(
+                                                    highlightColor: Colors.transparent,
+                                                    child: Row(
+                                                      mainAxisAlignment: MainAxisAlignment.center,
+                                                      children: [
+                                                        Icon(Icons.find_in_page_outlined),
+                                                        Text(" Select Image"),
+                                                      ],
+                                                    ),
+                                                    onPressed: () {
+                                                      HTML.FileUploadInputElement picker = HTML.FileUploadInputElement()..accept = 'image/*';
+                                                      picker.click();
+
+                                                      picker.onChange.listen((event) {
+                                                        setState(() {
+                                                          file = picker.files?.first;
+                                                        });
+                                                        context.showSuccessBar(
+                                                            content: Row(
+                                                              children: [
+                                                                const Text("Image selected successfully - "),
+                                                                Text(file!.name, style: const TextStyle(fontWeight: FontWeight.bold),),
+                                                              ],
+                                                            ),
+                                                            duration: Duration(seconds: 5)
+                                                        );
+                                                        final reader = HTML.FileReader();
+                                                        reader.readAsDataUrl(file!);
+                                                        reader.onLoadEnd.listen((event) {
+
+                                                        });
+                                                      });
+                                                    },
+                                                  ),
+                                                ),
+                                              ),
+                                              SizedBox(width: 10,),
+                                              Expanded(
+                                                child: Container(
+                                                  margin: EdgeInsets.symmetric(vertical: 35),
+                                                  decoration: BoxDecoration(
+                                                      borderRadius: const BorderRadius.all(Radius.circular(5.0)),
+                                                      color: Colors.red.shade900
+                                                  ),
+                                                  child: MaterialButton(
+                                                    highlightColor: Colors.transparent,
+                                                    child: Row(
+                                                      mainAxisAlignment: MainAxisAlignment.center,
+                                                      children: [
+                                                        Icon(Icons.upload_file),
+                                                        Text(" Upload"),
+                                                      ],
+                                                    ),
+                                                    onPressed: () async {
+                                                      if (file == null) {
+                                                        context.showErrorBar(content: Text("Pick an Image"));
+                                                        return;
+                                                      }
+                                                      if (therapists[i].uid != FirebaseAuth.instance.currentUser!.uid) {
+                                                        context.showErrorBar(content: Text("You can only change your image"));
+                                                        return;
+                                                      }
+                                                      setState(() {
+                                                        loader = true;
+                                                      });
+                                                      CollectionReference users = FirebaseFirestore.instance.collection('users');
+                                                      String link = "";
+                                                      final datTime = DateTime.now().toIso8601String();
+                                                      final userID = FirebaseAuth.instance.currentUser?.uid;
+                                                      final path = '$userID/$datTime';
+                                                      FB.UploadTaskSnapshot task = await FB.storage().refFromURL('gs://wilvcdn2021.appspot.com').child(path).put(file).future;
+                                                      Uri uri = await task.ref.getDownloadURL();
+                                                      link = uri.toString();
+                                                      PostImage image = PostImage(id: task.ref.name, url: link);
+                                                      users.doc(FirebaseAuth.instance.currentUser!.uid).update({
+                                                        'pic': image.url,
+                                                      }).then((value) {
+                                                        List<UserModel> users = [];
+                                                        FirebaseFirestore.instance.collection('users').get().then((value) {
+                                                          value.docs.forEach((element) {
+                                                            users.add(UserModel.fromJson(element.data()));
+                                                          });
+                                                          users.forEach((element) {
+                                                            if (element.admin) {
+                                                              therapists.add(element);
+                                                            }
+                                                          });
+                                                          setState(() {
+                                                            loader = false;
+                                                          });
+                                                        });
+                                                      });
+                                                    },
+                                                  ),
+                                                ),
+                                              ),
+                                              SizedBox(width: 10,),
+                                            ],
+                                          )
                                         ],
                                       ),
                                   )
@@ -136,5 +252,51 @@ class _ContactUsState extends State<ContactUs> {
     setState(() {
       therapists = list;
     });
+  }
+
+  //this method only upload the image and get the image download url
+  Future<PostImage> uploadImage() async {
+    String link = "";
+    final datTime = DateTime.now().toIso8601String();
+    final userID = FirebaseAuth.instance.currentUser?.uid;
+    final path = '$userID/$datTime';
+    FB.UploadTaskSnapshot task = await FB.storage().refFromURL('gs://wilvcdn2021.appspot.com').child(path).put(file).future;
+    Uri uri = await task.ref.getDownloadURL();
+    link = uri.toString();
+    PostImage image = PostImage(id: task.ref.name, url: link);
+    return image;
+  }
+
+  Future<bool> uploadPost(String email, String password, String name, String contact, String link) async {
+    bool task = false;
+    CollectionReference usersRef = FirebaseFirestore.instance.collection('users');
+    User? user = FirebaseAuth.instance.currentUser;
+
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password);
+      user = userCredential.user;
+
+      //once user has been registered
+      usersRef.doc(user!.uid).set({
+        'uid': user.uid,
+        'admin': true,
+        'contact': contact,
+        'pic': link,
+        'name': name
+      });
+      await user.updateDisplayName(name);
+      task = true;
+    }  on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        context.showErrorBar(content: Text("The password provided is too weak."));
+      } else if (e.code == 'email-already-in-use') {
+        context.showErrorBar(content: Text("The account already exists for that email."));
+      }
+      return task;
+    } catch (e) {
+      print(e);
+    }
+
+    return task;
   }
 }
